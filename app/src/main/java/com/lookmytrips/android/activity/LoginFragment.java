3e5cc6bd8769da9e74c9c7e1ba390b223a9ede39
Mark james <mjrun77@gmail.com>
2016-09-07 17:04:30 +0300

    Initial commit

---
+++ app/src/main/java/com/lookmytrips/android/activity/LoginFragment.java
@@ -0,0 +1,195 @@
+package com.lookmytrips.android.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.gorbin.asne.core.SocialNetwork;
+import com.github.gorbin.asne.core.SocialNetworkManager;
+import com.github.gorbin.asne.core.listener.OnLoginCompleteListener;
+import com.github.gorbin.asne.facebook.FacebookSocialNetwork;
+import com.github.gorbin.asne.googleplus.GooglePlusSocialNetwork;
+import com.github.gorbin.asne.vk.VkSocialNetwork;
+import com.lookmytrips.android.R;
+import com.vk.sdk.VKScope;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginFragment extends Fragment implements SocialNetworkManager.OnInitializationCompleteListener, OnLoginCompleteListener {
+    public static SocialNetworkManager mSocialNetworkManager;
+
+    private Button facebook;
+    private Button vk;
+    private Button odnoklassniki;
+    private Button googleplus;
+    private TextView tvLookWithout;
+
+    public LoginFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.login_fragment, container, false);
+        ((LoginActivity)getActivity()).getSupportActionBar().setTitle(R.string.app_name);
+        // init buttons and set Listener
+        facebook = (Button) rootView.findViewById(R.id.facebook);
+        facebook.setOnClickListener(loginClick);
+        vk = (Button) rootView.findViewById(R.id.vk);
+        vk.setOnClickListener(loginClick);
+        tvLookWithout = (TextView) rootView.findViewById(R.id.tv_without_auth);
+        tvLookWithout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), MainActivity.class));
+            }
+        });
+//        odnoklassniki = (Button) rootView.findViewById(R.id.ok);
+//        odnoklassniki.setOnClickListener(loginClick);
+        googleplus = (Button) rootView.findViewById(R.id.googleplus);
+        googleplus.setOnClickListener(loginClick);
+
+        //Get Keys for initiate SocialNetworks
+        String VK_KEY = getActivity().getString(R.string.vk_app_id);
+
+        //Chose permissions
+        ArrayList<String> fbScope = new ArrayList<String>();
+        fbScope.addAll(Arrays.asList("public_profile, email, user_friends"));
+        String linkedInScope = "r_basicprofile+r_fullprofile+rw_nus+r_network+w_messages+r_emailaddress+r_contactinfo";
+
+        //Use manager to manage SocialNetworks
+        mSocialNetworkManager = (SocialNetworkManager) getFragmentManager().findFragmentByTag(LoginActivity.SOCIAL_NETWORK_TAG);
+
+        //Check if manager exist
+        if (mSocialNetworkManager == null) {
+            mSocialNetworkManager = new SocialNetworkManager();
+
+            //Init and add to manager FacebookSocialNetwork
+            FacebookSocialNetwork fbNetwork = new FacebookSocialNetwork(this, fbScope);
+            mSocialNetworkManager.addSocialNetwork(fbNetwork);
+
+            //Init and add to manager TwitterSocialNetwork
+            String[] vkScope = new String[] {
+                    VKScope.FRIENDS,
+                    VKScope.WALL,
+                    VKScope.PHOTOS,
+                    VKScope.NOHTTPS,
+                    VKScope.STATUS,
+            };
+            VkSocialNetwork vkNetwork = new VkSocialNetwork(this, VK_KEY, vkScope);
+            mSocialNetworkManager.addSocialNetwork(vkNetwork);
+//
+//            //Init and add to manager LinkedInSocialNetwork
+//            LinkedInSocialNetwork liNetwork = new LinkedInSocialNetwork(this, LINKEDIN_CONSUMER_KEY, LINKEDIN_CONSUMER_SECRET, LINKEDIN_CALLBACK_URL, linkedInScope);
+//            mSocialNetworkManager.addSocialNetwork(liNetwork);
+
+            //Init and add to manager LinkedInSocialNetwork
+            GooglePlusSocialNetwork gpNetwork = new GooglePlusSocialNetwork(this);
+            mSocialNetworkManager.addSocialNetwork(gpNetwork);
+
+            //Initiate every network from mSocialNetworkManager
+            getFragmentManager().beginTransaction().add(mSocialNetworkManager, LoginActivity.SOCIAL_NETWORK_TAG).commit();
+            mSocialNetworkManager.setOnInitializationCompleteListener(this);
+        } else {
+            //if manager exist - get and setup login only for initialized SocialNetworks
+            if(!mSocialNetworkManager.getInitializedSocialNetworks().isEmpty()) {
+                List<SocialNetwork> socialNetworks = mSocialNetworkManager.getInitializedSocialNetworks();
+                for (SocialNetwork socialNetwork : socialNetworks) {
+                    socialNetwork.setOnLoginCompleteListener(this);
+  //                  initSocialNetwork(socialNetwork);
+                }
+            }
+        }
+        return rootView;
+    }
+
+//    private void initSocialNetwork(SocialNetwork socialNetwork){
+//        if(socialNetwork.isConnected()){
+//            switch (socialNetwork.getID()){
+//                case FacebookSocialNetwork.ID:
+//                    facebook.setText("Show Facebook profile");
+//                    break;
+//                case VkSocialNetwork.ID:
+//                    vk.setText("Show VK profile");
+//                    break;
+////                case LinkedInSocialNetwork.ID:
+////                    odnoklassniki.setText("Show LinkedIn profile");
+////                    break;
+//                case GooglePlusSocialNetwork.ID:
+//                    googleplus.setText("Show GooglePlus profile");
+//                    break;
+//            }
+//        }
+//    }
+    @Override
+    public void onSocialNetworkManagerInitialized() {
+        //when init SocialNetworks - get and setup login only for initialized SocialNetworks
+        for (SocialNetwork socialNetwork : mSocialNetworkManager.getInitializedSocialNetworks()) {
+            socialNetwork.setOnLoginCompleteListener(this);
+  //          initSocialNetwork(socialNetwork);
+        }
+    }
+
+    //Login listener
+
+    private View.OnClickListener loginClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int networkId = 0;
+            switch (view.getId()){
+                case R.id.facebook:
+                    networkId = FacebookSocialNetwork.ID;
+                    break;
+                case R.id.vk:
+                    networkId = VkSocialNetwork.ID;
+                    break;
+//                case R.id.odnoklassniki:
+//                    networkId = LinkedInSocialNetwork.ID;
+//                    break;
+                case R.id.googleplus:
+                    networkId = GooglePlusSocialNetwork.ID;
+                    break;
+            }
+            SocialNetwork socialNetwork = mSocialNetworkManager.getSocialNetwork(networkId);
+            if(!socialNetwork.isConnected()) {
+                if(networkId != 0) {
+                    socialNetwork.requestLogin();
+                    LoginActivity.showProgress("Loading social person");
+                } else {
+                    Toast.makeText(getActivity(), "Wrong networkId", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                startProfile(socialNetwork.getID());
+            }
+        }
+    };
+
+    @Override
+    public void onLoginSuccess(int networkId) {
+        LoginActivity.hideProgress();
+        startProfile(networkId);
+    }
+
+    @Override
+    public void onError(int networkId, String requestID, String errorMessage, Object data) {
+        LoginActivity.hideProgress();
+        Toast.makeText(getActivity(), "ERROR: " + errorMessage, Toast.LENGTH_LONG).show();
+    }
+
+    private void startProfile(int networkId){
+        ProfileFragment profile = ProfileFragment.newInstannce(networkId);
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .addToBackStack("profile")
+                .replace(R.id.container, profile)
+                .commit();
+    }
+
+}
---
+++ app/src/main/java/com/lookmytrips/android/activity/MainActivity.java
@@ -0,0 +1,444 @@
+package com.lookmytrips.android.activity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.speech.RecognizerIntent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.lookmytrips.android.R;
+import com.lookmytrips.android.adapters.ViewPagerAdapter;
+import com.lookmytrips.android.fragments.FeedInterestingFragment;
+import com.lookmytrips.android.fragments.FeedNewFragment;
+import com.lookmytrips.android.fragments.FeedPopularFragment;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import br.com.mauker.materialsearchview.MaterialSearchView;
+
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ImageButton a;
+    private MaterialSearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.nav_photo);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        a = (ImageButton)findViewById(R.id.a);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+
+            }
+        });
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        searchView.setSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewOpened() {
+                // Do something once the view is open.
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                // Do something once the view is closed.
+            }
+        });
+
+        searchView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Do something when the suggestion list is clicked.
+                String suggestion = searchView.getSuggestionAtPosition(position);
+
+                searchView.setQuery(suggestion, false);
+            }
+        });
+
+//        searchView.setTintAlpha(200);
+        searchView.adjustTintAlpha(0.8f);
+
+        final Context context = this;
+        searchView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(context, "Long clicked position: " + i, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        searchView.setOnVoiceClickedListener(new MaterialSearchView.OnVoiceClickedListener() {
+            @Override
+            public void onVoiceClicked() {
+                Toast.makeText(context, "Voice clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new FeedPopularFragment(), getResources().getString(R.string.feedPopularFragment));
+        adapter.addFrag(new FeedInterestingFragment(), getResources().getString(R.string.feedInterestingFragment));
+        adapter.addFrag(new FeedNewFragment(), getResources().getString(R.string.feedNewFragment));
+        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+
+
+        } else {
+            super.onBackPressed();
+        }
+
+        if (searchView.isOpen()) {
+            // Close the search on the back button press.
+            searchView.closeSearch();
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (item.getItemId()){
+            case R.id.action_bell:
+                Toast.makeText(this, "Soon", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_search:
+                searchView.openSearch();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_photo) {
+            // Handle the camera action
+        } else if (id == R.id.nav_countries) {
+
+        } else if (id == R.id.nav_interests) {
+
+        } else if (id == R.id.nav_contacts) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_logout) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private File savebitmap(Bitmap bmp) {
+        String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
+        OutputStream outStream = null;
+        // String temp = null;
+        File file = new File(extStorageDirectory, "temp.png");
+        if (file.exists()) {
+            file.delete();
+            file = new File(extStorageDirectory, "temp.png");
+
+        }
+
+        try {
+            outStream = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+            outStream.flush();
+            outStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return file;
+    }
+
+    private void selectImage() {
+
+        final CharSequence[] options = { "Сфотографировать", "Выбрать из галереи","Отмена" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Добавить фото!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (options[item].equals("Сфотографировать")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(Environment.getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    //pic = f;
+                    startActivityForResult(intent, 1);
+
+                }
+
+                else if (options[item].equals("Выбрать из галереи")) {
+                    Intent intent = new   Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+
+                }
+
+                else if (options[item].equals("Отмена")) {
+                    dialog.dismiss();
+
+                }
+
+            }
+
+        });
+
+        builder.show();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        searchView.clearSuggestions();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        searchView.activityResumed();
+        String[] arr = getResources().getStringArray(R.array.query_suggestions);
+
+        searchView.addSuggestions(arr);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//search
+        if (requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (matches != null && matches.size() > 0) {
+                String searchWrd = matches.get(0);
+                if (!TextUtils.isEmpty(searchWrd)) {
+                    searchView.setQuery(searchWrd, false);
+                }
+            }
+
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //for photos
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == 1) {
+                //h=0;
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+
+                for (File temp : f.listFiles()) {
+
+                    if (temp.getName().equals("temp.jpg")) {
+
+                        f = temp;
+                        File photo = new File(Environment.getExternalStorageDirectory(), "temp.jpg");
+                        //pic = photo;
+                        break;
+
+                    }
+
+                }
+
+                try {
+
+                    Bitmap bitmap;
+
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+
+                            bitmapOptions);
+
+
+                    a.setImageBitmap(bitmap);
+
+
+                    String path = Environment
+
+                            .getExternalStorageDirectory()
+
+                            + File.separator
+
+                            + "Phoenix" + File.separator + "default";
+                    //p = path;
+
+                    f.delete();
+
+                    OutputStream outFile = null;
+
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+
+                    try {
+
+                        outFile = new FileOutputStream(file);
+
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        //pic=file;
+                        outFile.flush();
+
+                        outFile.close();
+
+
+                    } catch (FileNotFoundException e) {
+
+                        e.printStackTrace();
+
+                    } catch (IOException e) {
+
+                        e.printStackTrace();
+
+                    } catch (Exception e) {
+
+                        e.printStackTrace();
+
+                    }
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+
+                }
+
+            } else if (requestCode == 2) {
+
+
+                Uri selectedImage = data.getData();
+                // h=1;
+                //imgui = selectedImage;
+                String[] filePath = {MediaStore.Images.Media.DATA};
+
+                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+
+                c.moveToFirst();
+
+                int columnIndex = c.getColumnIndex(filePath[0]);
+
+                String picturePath = c.getString(columnIndex);
+
+                c.close();
+
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+
+
+                Log.w("path of image", picturePath + "");
+
+
+                a.setImageBitmap(thumbnail);
+
+            }
+
+        }
+    }
+}
